363ba9f2ca86e6890809111e30c5c85e1ec084cd
TuncayArican <tuncayarican07@gmail.com>
2022-02-15 22:32:58 +0300

    Bug fixed

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ let to_do1=document.getElementById("to_do1");
 let confirm1=document.getElementById("confirm1");
 let h1=document.getElementById("h1");
 let task_list=document.getElementById("task_list");
-let to_do2=document.createElement("input");
-let remove1=document.createElement("button");
+
+
 
 confirm1.addEventListener("click", ()=>{
     h1.style.display="block"
     let to_do2=document.createElement("span");
     let remove1=document.createElement("button");
     to_do2.setAttribute("class", "to_do2");
-    remove1.setAttribute("class", "remove");
+    remove1.setAttribute("class", "remove1");
     to_do2.innerText=to_do1.value;
     to_do1.value=""
     remove1.innerText="X"
@@ -18,6 +18,7 @@ confirm1.addEventListener("click", ()=>{
     task_list.append(remove1);
     to_do2.addEventListener("click", ()=>{
         to_do2.style.textDecorationLine="line-through";
+        to_do2.style.textDecorationColor="red";
 
     }) 
     remove1.addEventListener("click", ()=>{
@@ -25,7 +26,6 @@ confirm1.addEventListener("click", ()=>{
         to_do2.style.display="none";
     }) 
 })
-
 
 
 
